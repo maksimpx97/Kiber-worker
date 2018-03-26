d9853cfea8acb510baee86786a6fad318eb53aea
maksimpx97 <maksimpx97@gmail.com>
2018-03-26 23:40:31 +0300

    Start project:)

---
+++ Assets/scripts/movePlayer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float playerSpeed;
+    public float jumpPower;
+    public int directionInput;
+    public bool groundCheck;
+    public bool facingRight = true;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        if ((directionInput < 0) && (facingRight))
+        {
+            Flip();
+        }
+
+        if ((directionInput > 0) && (!facingRight))
+        {
+            Flip();
+        }
+        groundCheck = true;
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(playerSpeed * directionInput, rb2d.velocity.y);
+    }
+
+    public void Move(int InputAxis)
+    {
+
+        directionInput = InputAxis;
+
+    }
+
+    public void Jump(bool isJump)
+    {
+        isJump = groundCheck;
+
+        if (groundCheck)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+}
